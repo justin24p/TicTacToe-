ea548054a2530e4deccdbe517735220c4caf48ef
justinp24 <justinpardo2423@gmail.com>
2024-03-08 00:57:08 -0500

    Implementd helper function to check for possible winning conditions

--- script.js
+++ script.js
@@ -9,26 +9,17 @@ function gameboard() {
     }
     const getboard = () => board;
 
-    const printboard = () => {
-        const boardWithCellValues = board.map((row) =>
-            row.map((cell) => cell.getValue())
-        );
-        console.log(boardWithCellValues);
-    };
     // this method will handel
-    const dropToken = (index, player) => {
-        const row = Math.floor((index - 1) / 3);
-        const collumn = index % 3;
-        if (board[row][collumn].getValue() === 0) {
-            board[row][collumn].addToken(player);
-            return true;
+    const dropToken = (mainindex, index, player) => {
+        if (board[mainindex][index].getValue() === 0) {
+            board[mainindex][index].addToken(player);
         } else {
-            return false;
+            return;
         }
         // implement drop token function
     };
 
-    return { getboard, printboard, dropToken };
+    return { getboard, dropToken };
 }
 
 // i need to create way that will allow me to alter the values of cell
@@ -50,22 +41,17 @@ function Cell() {
 function GameController() {
     const board = gameboard();
 
-    let printNewRound = () => {
-        console.log(`${getActivePlayer().name}'s turn.`);
-        board.printboard();
-    };
-
-    const playerOneName = "playername1";
-    const playerTwoName = "playername2";
+    const playerOneName = "X";
+    const playerTwoName = "O";
 
     const player = [
         {
             name: playerOneName,
-            token: 1,
+            token: "X",
         },
         {
             name: playerTwoName,
-            token: 2,
+            token: "O",
         },
     ];
 
@@ -77,36 +63,94 @@ function GameController() {
         ActivePlayer = ActivePlayer === player[0] ? player[1] : player[0];
     };
 
-    const checkifWinner = () => {};
-
-    const playRound = (gridspot) => {
-        if (board.dropToken(gridspot, getActivePlayer().token)) {
-            switchPlayer();
-            printNewRound();
-            checkifWinner();
-        } else {
-            handleUserInput();
+    const checkifWinner = () => {
+        let gameboard = board.getboard();
+        let matrixlength = gameboard.length - 1;
+        // rows
+        function helperWinner(array) {
+            if (array.every((element) => element.getValue() === "X")) {
+                return true;
+            }
         }
-    };
-    printNewRound();
+        // Handle Rows
+        gameboard.forEach((array) => {
+            helperWinner(array);
+        });
 
-    const handleUserInput = () => {
-        const input = prompt("Please choose a grid spot from 1-9: ");
-        const number = parseInt(input);
-        console.log(number);
-        if (number > 0 && number < 9 && !isNaN(number)) {
-            playRound(number);
-        } else {
-            handleUserInput();
+        // Handle Collumns
+        for (let i = 0; i < 3; i++) {
+            let collumn = gameboard.map((element) => {
+                return element[i];
+            });
+            helperWinner(collumn) ? true : false;
         }
-    };
-    let counter = 1;
-    // loop to run game
-    while (counter < 9) {
-        handleUserInput();
-        counter++;
-    }
+        // Handle Diagonals
+        let diagonalone = gameboard.map((array, index) => {
+            return array[index];
+        });
+        helperWinner(diagonalone) ? true : false;
 
-    return { getActivePlayer, playRound };
+        let diagonaltwo = gameboard.map((array) => {
+            return array[matrixlength--];
+        });
+        helperWinner(diagonaltwo) ? true : false;
+    };
+
+    const playRound = (row, collumn) => {
+        board.dropToken(row, collumn, getActivePlayer().token);
+        switchPlayer();
+        // if checkwinner call the overlay theme
+        checkifWinner() ? console.log("winner for x") : console.log("no win");
+    };
+    return { getActivePlayer, playRound, getboard: board.getboard };
 }
 playgame = GameController();
+
+const ScreenController = () => {
+    const game = GameController();
+    const board_div = document.querySelector(".board");
+    const player_div = document.querySelector(".player");
+
+    const updateScreen = () => {
+        board_div.textContent = "";
+
+        const activeplayer = game.getActivePlayer(".player");
+        const board = game.getboard();
+
+        player_div.textContent = `${activeplayer.name}'s Turn`;
+
+        const playercolor = (num) => {
+            if (num === "X") {
+                return "red";
+            } else if (num === "O") {
+                return "yellow";
+            } else return "black";
+        };
+        board.forEach((row, mainindex) => {
+            row.forEach((cell, index) => {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.textContent = cell.getValue() ? cell.getValue() : "";
+                cellButton.style.color = playercolor(cell.getValue());
+                cellButton.dataset.row = mainindex;
+                cellButton.dataset.collumn = index;
+
+                board_div.appendChild(cellButton);
+            });
+        });
+    };
+
+    function clickHandlerBoard(event) {
+        selectedcolumn = event.target.dataset.collumn;
+        selectedrow = event.target.dataset.row;
+        if (!selectedcolumn) return;
+
+        game.playRound(selectedrow, selectedcolumn);
+        updateScreen();
+    }
+    board_div.addEventListener("click", clickHandlerBoard);
+
+    updateScreen();
+};
+
+ScreenController();
